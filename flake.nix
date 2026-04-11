fab53e26b19de0806e0c4c17c19253eae4e56706
gomazarashi <gomazarashi0426@gmail.com>
2026-04-12 01:49:04 +0900

    nix関連

---
+++ flake.nix
@@ -0,0 +1,38 @@
+# flake.nix
+{
+  description = "gomazarashi.github.io development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    tola.url = "github:tola-ssg/tola-ssg/v0.7.1";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, tola }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            typst
+            just
+            git
+            ripgrep
+            curl
+            alejandra
+            tola.packages.${system}.default
+          ];
+
+          shellHook = ''
+            echo "devShell ready"
+            echo "available commands: just build | just serve | just clean"
+          '';
+        };
+
+        formatter = pkgs.alejandra;
+      });
+}
