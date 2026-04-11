285da7efa6fa041418d7c3fec99ac0314ae9a88f
gomazarashi <gomazarashi0426@gmail.com>
2026-04-12 02:24:20 +0900

    Typstのバージョンが古かったので更新

--- flake.nix
+++ flake.nix
@@ -4,26 +4,31 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     tola.url = "github:tola-ssg/tola-ssg/v0.7.1";
   };
 
-  outputs = { self, nixpkgs, flake-utils, tola }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, tola }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        unstablePkgs = import nixpkgs-unstable {
+          inherit system;
+        };
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            typst
-            just
-            git
-            ripgrep
-            curl
-            alejandra
+          packages = [
+            unstablePkgs.typst
+            pkgs.just
+            pkgs.git
+            pkgs.ripgrep
+            pkgs.curl
+            pkgs.alejandra
             tola.packages.${system}.default
           ];
 
